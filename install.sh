6e4c3b7e17f4f658fe64d0942dc26c4c60720f46
erdal2021 <erdal.erdogan@t-online.de>
2023-11-17 16:56:01 +0100

    install.sh editiert

--- install.sh
+++ install.sh
@@ -12,28 +12,3 @@ sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_passwo
 
 # Erstelle die Datenbank 'starchaser' und den Benutzer 'Hans' mit dem Passwort 'Dampfmaschine4711'
 sudo mysql -e "CREATE DATABASE starchaser; CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711'; GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost'; FLUSH PRIVILEGES;"
-2. requirements.txt (Liste der Python-Bibliotheken):
-plaintext
-Copy code
-sqlalchemy==1.4.25
-mysql-connector-python==8.0.26
-3. main.py (Python-Datei mit der main-Funktion):
-python
-Copy code
-# Importiere benötigte Module
-from pydantic import BaseModel
-
-# Definition der Person Klasse
-class Person(BaseModel):
-    firstname: str
-    lastname: str
-    email: str
-
-# Definition der main-Funktion
-def main():
-    # Ausgabe der gewünschten Nachricht
-    print("AWS23/07")
-
-# Prüfe, ob die main-Funktion direkt aufgerufen wird (nicht als Modul importiert)
-if __name__ == "__main__":
-    main()
