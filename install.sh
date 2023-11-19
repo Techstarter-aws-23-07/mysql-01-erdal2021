1f07d423dd0e98cc86e7ba874e4b8fb15b3a2c6b
erdal2021 <87144653+erdal2021@users.noreply.github.com>
2023-11-19 01:20:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,26 @@ sudo apt-get install -y mysql-server
 # Starte den MySQL-Server
 sudo service mysql start
 
+# Einloggen in MySQL als Root-Benutzer
+sudo mysql -u root -p
+
+# Nach Eingabe des Root-Passworts in der MySQL-Shell.
+
+# Erstellen der Datenbank 'starchaser'
+CREATE DATABASE starchaser;
+
+# Erstellen des Benutzers 'Hans' mit dem Passwort 'Dampfmaschine4711'
+CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711';
+
+# Berechtigungen für den Benutzer 'Hans' auf die Datenbank 'starchaser' gewähren
+GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost';
+
+# Änderungen übernehmen
+FLUSH PRIVILEGES;
+
+# Verlassen der MySQL-Shell
+exit
+
+
 # Erstelle die Datenbank 'starchaser' und den Benutzer 'Hans' mit dem Passwort 'Dampfmaschine4711'
-sudo mysql -e "CREATE DATABASE starchaser; CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711'; GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost'; FLUSH PRIVILEGES;"
+# sudo mysql -e "CREATE DATABASE starchaser; CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711'; GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost'; FLUSH PRIVILEGES;"
