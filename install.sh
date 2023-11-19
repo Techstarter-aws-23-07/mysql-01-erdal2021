e8f8786ec007b1361a0981240e82714849f74016
erdal2021 <87144653+erdal2021@users.noreply.github.com>
2023-11-19 01:38:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,28 +8,28 @@ sudo apt-get install -y mysql-server
 sudo service mysql start
 
 # Setze das MySQL-Root-Passwort auf 'root'
-# sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;"
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;"
+
+# Erstelle die Datenbank 'starchaser' und den Benutzer 'Hans' mit dem Passwort 'Dampfmaschine4711'
+sudo mysql -e "CREATE DATABASE starchaser; CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711'; GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost'; FLUSH PRIVILEGES;"
+
 
 # Einloggen in MySQL als Root-Benutzer
-sudo mysql -u root -p
+# sudo mysql -u root -p
 
 # Nach Eingabe des Root-Passworts in der MySQL-Shell.
 
 # Erstellen der Datenbank 'starchaser'
-CREATE DATABASE starchaser;
+# CREATE DATABASE starchaser;
 
 # Erstellen des Benutzers 'Hans' mit dem Passwort 'Dampfmaschine4711'
-CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711';
+# CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711';
 
 # Berechtigungen für den Benutzer 'Hans' auf die Datenbank 'starchaser' gewähren
-GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost';
+# GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost';
 
 # Änderungen übernehmen
-FLUSH PRIVILEGES;
+# FLUSH PRIVILEGES;
 
 # Verlassen der MySQL-Shell
-exit
-
-
-# Erstelle die Datenbank 'starchaser' und den Benutzer 'Hans' mit dem Passwort 'Dampfmaschine4711'
-# sudo mysql -e "CREATE DATABASE starchaser; CREATE USER 'Hans'@'localhost' IDENTIFIED BY 'Dampfmaschine4711'; GRANT ALL PRIVILEGES ON starchaser.* TO 'Hans'@'localhost'; FLUSH PRIVILEGES;"
+# exit
